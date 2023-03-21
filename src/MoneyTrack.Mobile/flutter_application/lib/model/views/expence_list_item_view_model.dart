a0d1ee534cbcc0ac3c1b58f0e64d7d1edd4e9ec4
Artem Langavyi <Artem.Langavyi@poort80.nl>
2023-03-21 15:50:40 +0100

    added card for monthi orders overview

--- src/MoneyTrack.Mobile/flutter_application/lib/model/views/expence_list_item_view_model.dart
+++ src/MoneyTrack.Mobile/flutter_application/lib/model/views/expence_list_item_view_model.dart
@@ -2,13 +2,16 @@ class ExpenceListItemViewMode {
   final String _title;
   final double _amount;
   final int _orderId;
-
-  ExpenceListItemViewMode({required title, required amount, required orderId})
+  final DateTime _orderDate;
+  ExpenceListItemViewMode(
+      {required title, required amount, required orderId, required orderDate})
       : _title = title,
         _amount = amount,
-        _orderId = orderId;
+        _orderId = orderId,
+        _orderDate = orderDate;
 
   String get title => _title;
   double get amount => _amount;
   int get orderId => _orderId;
+  DateTime get orderDate => _orderDate;
 }
--- src/MoneyTrack.Mobile/flutter_application/lib/widgets/Expences/expence_list_by_period.dart
+++ src/MoneyTrack.Mobile/flutter_application/lib/widgets/Expences/expence_list_by_period.dart
@@ -8,16 +8,27 @@ class ExpenceListByPeriodView extends StatelessWidget {
   final List<ExpenceListItemViewMode> expences = List.generate(
     20,
     (index) => ExpenceListItemViewMode(
-        title: "Expence Item Overview Title", amount: 10.55, orderId: 0),
+        title: "Expence Item Overview Title",
+        amount: 10.55,
+        orderId: 0,
+        orderDate: DateTime.now()),
   );
 
   ExpenceListByPeriodView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        height: 200,
-        child: Card(
+    return Card(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const ListTile(
+          leading: Icon(Icons.monetization_on_outlined),
+          title: Text('Expences for last month'),
+          subtitle: Text('This is list of order for current month'),
+        ),
+        SizedBox(
+            height: 188,
             child: ListView.builder(
                 itemCount: expences.length,
                 prototypeItem: ListTile(
@@ -28,11 +39,13 @@ class ExpenceListByPeriodView extends StatelessWidget {
                     onTap: () async {},
                     leading: const Icon(Icons.payments_outlined),
                     title: Text(
-                      expences[index].title,
+                      "${expences[index].title} ${expences[index].amount}  ${expences[index].orderDate.toString()}",
                       style: const TextStyle(fontSize: 20),
                     ),
                     subtitle: const Text('TBD: maybe some subtitle?'),
                   );
-                })));
+                })),
+      ],
+    ));
   }
 }
